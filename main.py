feb0a7d3c8e457d9eab9fadd5ce2558fcc3f770b
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 21:36:28 -0300

    more exercises

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ if(2 + 4 + 6 > 12):
     print("2 + 4 + 6 is higher than 12")
 else:
     print("2+4+6 does not equal or is higher than 12")
+if(1387%19 == 0):
+    print("1387 can be divided by 19")
+else:
+    print("1387 cannot be divided by 19")
+if(31%2 == 0):
+    print("31 is pair")
+else:
+    print("31 isn't par")
+if(min(32, 29, 31) < 30):
+    print("the lowest value between 34, 29 and 31  is lower than 30")
+else:
+    print("30 is higher or equal to the lowest number between 34, 29 and 31")
