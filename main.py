74ab789428d51a27f8df8aae0952af236c54adce
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 21:22:41 -0300

    fixed an operation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if(3**2 + 4**2 == 25):
     print("3**2 + 4**2 equals 25")
 else:
     print("2**2 + 4**2 does not equal 25")
-if(2 + 4 + 6 == 12):
+if(2 + 4 + 6 > 12):
     print("2 + 4 + 6 equals 12")
 else:
     print("2+4+6 does not equal 12")
