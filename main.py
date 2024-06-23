d15694bb05637f739125bcb334551325c4c7a689
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 21:18:31 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+if(2 + 2 <4):
+    print("lower than 4.")
+else:
+    print("higher or equal to 4")
+if(7//3 == 1+1):
+    print("7//3 is equal to 1+1")
+else:
+    print("7//3 is not equal to 1+1")
+if(3**2 + 4**2 == 25):
+    print("3**2 + 4**2 equals 25")
+else:
+    print("2**2 + 4**2 does not equal 25")
+if(2 + 4 + 6 == 12):
+    print("2 + 4 + 6 equals 12")
+else:
+    print("2+4+6 does not equal 12")
